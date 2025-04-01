25899edcc0d1c1b71f81e0e403a2a34d032b2209
eloa <francodosreissilvae@gmail.com>
2025-04-01 11:26:48 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
+import React from 'react';
+import './App.css';
+import logo from './assets/Captura de tela 2025-04-01 112111.png';
 
 function App() {
- 
-
   return (
-    <>
-      <div>
+    <div>
+      <div className="top-bar">
+        <img src={logo} alt="Logo" />
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
