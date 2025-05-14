eac30ed26d26b0d722bf40e1f2f6c0980285b83d
eloasenai <158314951+eloasenai@users.noreply.github.com>
2025-05-14 18:21:46 +0000

    adicionando iconi perfil

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import logo from './assets/Captura de tela 2025-04-01 112111.png';
+import logo from './assets/Captura de tela 2025-04-01 112111.png'; // Logo
 
 function App() {
   return (
     <div>
-      <div className="top-bar">
-        <img src={logo} alt="Logo" />
+      <div className="top-bar d-flex justify-content-between align-items-center p-3">
+        <img src={logo} alt="Logo" className="logo" />
+        <i className="bi bi-person-circle perfil-icon"></i> {/* Ícone de perfil */}
       </div>
     </div>
   );
