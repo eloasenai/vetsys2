7d6a53b5b6cb724143020e9ef45bd77a97a94056
Pietra <pgodoifelipe@gmail.com>
2025-05-21 15:45:36 -0300

    adicionando botões

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router";
 
 import Header from "./components/header/Header";
 import Cadastro from "./pages/Cadastro";
+
 const AppWrapper = () => {
   const location = useLocation();
 
@@ -12,22 +13,52 @@ const AppWrapper = () => {
   const shouldHideHeader = hideHeaderRoutes.includes(location.pathname);
 
   return (
-    <>
-      {!shouldHideHeader && <Header />}
-      <Routes>
-        <Route path="/cadastro" element={<Cadastro />} />
-        {/* Outras rotas aqui */}
-      </Routes>
-    </>
-  );
-};
+      <>
+        {!shouldHideHeader && <Header />}
+  
+        {/* Botões abaixo do Header */}
+        {!shouldHideHeader && (
+          <div className="d-flex justify-content-center gap-3 py-3">
+            {["Gato", "Cachorro", "Pássaros", "Roedores", "Promoções"].map((label) => (
+              <div
+                key={label}
+                to={`/${label.toLowerCase()}`}
+                className="btn"
+                style={{
+                  border: "1px solid black",
+                  borderRadius: "20px",
+                  padding: "30px 60px",
+                  backgroundImage: `url(/assets/img/botão.png${label.toLowerCase()}.jpg)`, // Imagem de fundo
+                  backgroundSize: "cover", // Ajusta a imagem para cobrir o botão
+                  backgroundPosition: "center", // Centraliza a imagem
+                  margin: "5px",
+                }}
+              >
+                {label}
+              </div>
+            ))}
+          </div>
+        )}
+  
+        <Routes>
+          <Route path="/cadastro" element={<Cadastro />} />
+          {/* Outras rotas aqui */}
+        </Routes>
+      </>
+    );
+  };
 
 const App = () => {
   return (
     <BrowserRouter>
       <AppWrapper />
     </BrowserRouter>
+    
   );
 };
+<div className="container">
+  <h1>Cadastro</h1>
+  <p>Esta é a página de cadastro.</p></div>
+
 
 export default App;
