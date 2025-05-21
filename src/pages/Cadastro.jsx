c049be6b8c99d74676ea9b09f485fd7ed53accc0
eloa <francodosreissilvae@gmail.com>
2025-05-21 14:31:52 -0300

    arrumando cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -13,7 +13,10 @@ const Cadastro = () => {
   const handleCadastro = (e) => {
     e.preventDefault();
     if (nome && sobrenome && email && senha && confirmarsenha) {
-      localStorage.setItem("devcadastro", JSON.stringify({ nome, sobrenome, email, senha, confirmarsenha }));
+      localStorage.setItem(
+        "devcadastro",
+        JSON.stringify({ nome, sobrenome, email, senha, confirmarsenha })
+      );
       navigate("/"); // redireciona para a tela inicial
     }
   };
@@ -21,17 +24,36 @@ const Cadastro = () => {
   return (
     <div className="cadastro-container">
       {/* Imagem ao lado do formulário */}
-      <div className="cadastro-image" style={{ width: "100%", marginLeft: "20%", borderRadius: "80%" }}>
-        <img 
+      <div
+        className="cadastro-image"
+        style={{ width: "100%", marginLeft: "20%", borderRadius: "80%" }}
+      >
+        <img
           src="src/assets/img/cadastro.png" // Substitua pelo caminho da sua imagem
           alt="Cadastro"
         />
       </div>
       {/* Formulário de cadastro */}
       <div className="cadastro-form">
-        <div className="card" style={{ width: "90%",marginLeft: "-20%" }}>
-          <div className="card-body">
-            <h5 className="card-title">Bem-Vindo ao VetSys!</h5>
+        <div
+          className="card"
+          style={{
+            width: "170%",
+            marginLeft: "-80%",
+            borderColor: "black",
+            borderRadius: "20px",
+          }}
+        >
+          <div
+            className="card-body"
+            style={{ height: "80vh", padding: "80px", marginTop: "50px" }} // Adicionado marginTop
+          >
+            <h5
+              className="card-title text-center text-dark"
+              style={{ margin: "25px" }}
+            >
+              Bem-Vindo ao VetSys!
+            </h5>
             <form onSubmit={handleCadastro}>
               <div className="mb-3">
                 <label htmlFor="nome" className="form-label">
@@ -45,18 +67,19 @@ const Cadastro = () => {
                   value={nome}
                   onChange={(e) => setNome(e.target.value)}
                 />
-                 <div className="mb-3">
-                <label htmlFor="sobrenome" className="form-label">
-                  Sobrenome
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="sobrenome"
-                  value={sobrenome}
-                  onChange={(e) => setSobrenome(e.target.value)}
+                <div className="mb-3">
+                  <label htmlFor="sobrenome" className="form-label">
+                    Sobrenome
+                  </label>
+                  <input
+                    style={{ borderColor: "black" }}
+                    type="text"
+                    className="form-control"
+                    id="sobrenome"
+                    value={sobrenome}
+                    onChange={(e) => setSobrenome(e.target.value)}
                   />
-                  </div>
+                </div>
               </div>
               <div className="mb-3">
                 <label htmlFor="email" className="form-label">
@@ -76,6 +99,7 @@ const Cadastro = () => {
                   Senha
                 </label>
                 <input
+                  style={{ borderColor: "black" }}
                   type="password"
                   className="form-control"
                   id="senha"
@@ -88,6 +112,7 @@ const Cadastro = () => {
                   Confirmar Senha
                 </label>
                 <input
+                  style={{ borderColor: "black" }}
                   type="password"
                   className="form-control"
                   id="Confirmar senha"
@@ -95,7 +120,16 @@ const Cadastro = () => {
                   onChange={(e) => setConfirmarsenha(e.target.value)}
                 />
               </div>
-              <button type="submit" className="btn btn-primary">
+              <button
+                type="submit"
+                className="btn border-2"
+                style={{
+                  borderColor: "black",
+                  marginLeft: "43%",
+                  borderRadius: "15px",
+                  width: "15%",
+                }}
+              >
                 Cadastrar
               </button>
             </form>
