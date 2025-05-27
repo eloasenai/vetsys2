ef958ae9af82aa24b98af9d714db87e425035b87
Pietra <pgodoifelipe@gmail.com>
2025-05-27 08:11:20 -0300

    editando botões

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,34 @@ import Cadastro from "./pages/Cadastro";
 import Entrar from "./pages/Entrar";
 import Footer from "./components/footer/Footer";
 
+const AppWrapper = () => {
+  const location = useLocation();
 
-  const AppWrapper = () => {
-    const location = useLocation();
-  
-    // Ocultar o Header nas páginas "Entrar" e "Cadastro"
-    const hideHeaderRoutes = ["/entrar", "/cadastro"];
-    const shouldHideHeader = hideHeaderRoutes.includes(location.pathname);
+  // Ocultar o Header nas páginas "Entrar" e "Cadastro"
+  const hideHeaderRoutes = ["/entrar", "/cadastro"];
+  const shouldHideHeader = hideHeaderRoutes.includes(location.pathname);
 
- 
   return (
     <>
-   {!shouldHideHeader && <Header />} {/* Renderiza o Header apenas se não estiver nas rotas ocultas */}
+      {!shouldHideHeader && <Header />}
+
+      {/* Botões abaixo do Header */}
+     
+
       <Routes>
-        <Route path="/entrar" element={<Entrar />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        {/* Outras rotas aqui */}
       </Routes>
     </>
   );
 };
 
-   
-          
-        
 const App = () => {
   return (
     <BrowserRouter>
- <AppWrapper /> 
+      <AppWrapper />
     </BrowserRouter>
-    
   );
 };
-<div className="container">
-  <h1>Cadastro</h1>
-  <p>Esta é a página de cadastro.</p></div>
-
 
 export default App;
