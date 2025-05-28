5e7390d34b0394a276cf741e60e8ce6d8a5aaaa9
Bianca <bianca.furlanetto440@gmail.com>
2025-05-28 15:24:29 -0300

    produtosRoedores

--- src/pages/Roedores.jsx
+++ src/pages/Roedores.jsx
@@ -127,9 +127,8 @@ const Roedores = () => {
               info: [
                 "Ração premium para coelhos",
                 "Ingredientes: Feno, beterraba, alfafa",
-                "Tamanho: 1.2kg",
-                "Idade: Adultos",
-                "Preço: ",
+                "Peso: 1.5kg",
+                "Indicação: Coelhos adultos",
               ],
             },
             {
@@ -138,9 +137,8 @@ const Roedores = () => {
               info: [
                 "Ração Nutrópica para porquinhos-da-índia",
                 "Ingredientes: Feno, ervilha, alfafa",
-                "Tamanho: 1.2kg",
-                "Idade: Porquinhos-da-índia",
-                "Preço: ",
+                "Peso: 1.2kg",
+                "Indicação: Porquinhos-da-índia",
               ],
             },
             {
@@ -149,24 +147,23 @@ const Roedores = () => {
               info: [
                 "Ração Muesli para hamsters",
                 "Ingredientes: Grãos, sementes, frutas secas",
-                "Tamanho: 800g",
-                "Idade: Hamsters",
-                "Preço: ",
+                "Peso: 300g",
+                "Indicação: Hamsters",
               ],
             },
           ].map((item, index) => (
             <div
               key={index}
               style={{
-                width: "300px",
+                width: "320px",
                 height: "auto",
                 backgroundColor: "white",
                 borderRadius: "10px",
-                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+                boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
-                padding: "10px",
+                padding: "15px",
               }}
             >
               <img
@@ -180,14 +177,269 @@ const Roedores = () => {
                   fontSize: "14px",
                   color: "#333",
                   padding: "10px",
+                  listStyleType: "none",
                 }}
               >
                 {item.info.map((infoItem, infoIndex) => (
-                  <li key={infoIndex}>{infoItem}</li>
+                  <li key={infoIndex}>
+                    {infoItem.startsWith("Preço:") ? (
+                      <span style={{ fontWeight: "bold", color: "red" }}>{infoItem}</span>
+                    ) : (
+                      infoItem
+                    )}
+                  </li>
                 ))}
               </ul>
             </div>
           ))}
+        </div>
+        {/* PETISCOS */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "20px",
+            padding: "20px",
+          }}
+        >
+          <h1
+            style={{
+              color: "#FFBD00",
+              fontFamily: "Yeseva One",
+              fontSize: "70px",
+              marginBottom: "20px",
+              marginRight: "1300px",
+            }}
+          >
+            PETISCOS:
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: "20px",
+              justifyContent: "center",
+            }}
+          >
+            {[
+              {
+                src: "src/assets/img/Roedores.img/Petiscos-Funny-Bunny-18kg.png",
+                alt: "Petisco 1",
+                info: [
+                  "Funny Bunny Delícias da Horta Supra",
+                  "Ingredientes: Vegetais e grãos",
+                  "Tamanho: 1,8kg",
+                  "Idade: Todas as idades",
+                  "Preço: 52,90",
+                ],
+              },
+              {
+                src: "src/assets/img/Roedores.img/Petiscos2-Funny-Bunny-Blend-500g.png",
+                alt: "Petisco 2",
+                info: [
+                  " Funny Bunny Roedores Blend Supra",
+                  "Ingredientes: Vegetais e frutas secas",
+                  "Tamanho: 500g",
+                  "Idade: Todas as idades",
+                  "Preço: 18,76",
+                ],
+              },
+              {
+                src: "src/assets/img/Roedores.img/Petisco3_hamster_sticks_200g_casa_do_produtor.webp",
+                alt: "Petisco 3",
+                info: [
+                  " Alcon Hamster Sticks Para Pequenos Roedores ",
+                  "Ingredientes:Cereais,cenoura desidratada e milho",
+                  "Tamanho: 200g",
+                  "Idade: Adultos",
+                  "Preço: 18,90",
+                ],
+              },
+              {
+                src: "src/assets/img/Roedores.img/Petisco4.jpg",
+                alt: "Petisco 4",
+                info: [
+                  "Petisco Aves da Mata Biscotto Rodente Para Roedores",
+                  "Ingredientes:Cereais e sementes naturais ",
+                  "Tamanho: 20g",
+                  "Idade:Todas as idades ",
+                  "Preço: 14,99",
+                ],
+              },
+              {
+                src: "src/assets/img/Roedores.img/Petisco5.jpg",
+                alt: "Petisco  5",
+                info: [
+                  "Biscoito Flower Seeds",
+                  "Ingredientes:Amendoin e sementes ",
+                  "Tamanho: 80g",
+                  "Idade:Filhotes",
+                  "Preço:29,90 ",
+                ],
+              },
+            ].map((item, index) => (
+              <div
+                key={index}
+                style={{
+                  width: "320px",
+                  height: "auto",
+                  backgroundColor: "white",
+                  borderRadius: "10px",
+                  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  padding: "15px",
+                }}
+              >
+                <img
+                  src={item.src}
+                  alt={item.alt}
+                  style={{
+                    width: "250px",
+                    height: "250px",
+                    objectFit: "cover",
+                  }}
+                />
+                <ul
+                  style={{
+                    textAlign: "left",
+                    fontSize: "14px",
+                    color: "#333",
+                    padding: "10px",
+                    listStyleType: "none",
+                  }}
+                >
+                  {item.info.map((infoItem, infoIndex) => (
+                    <li key={infoIndex}>
+                      {infoItem.startsWith("Preço:") ? (
+                        <span style={{ fontWeight: "bold", color: "red" }}>{infoItem}</span>
+                      ) : (
+                        infoItem
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+           {/* ACESSÓRIOS */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "20px",
+            padding: "20px",
+          }}
+        > <h1
+        style={{
+          color: "#FFBD00",
+          fontFamily: "Yeseva One",
+          fontSize: "70px",
+          marginBottom: "20px",
+          marginRight: "1300px",
+        }}
+      >
+        ACESSÓRIOS:
+      </h1>
+      <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: "20px",
+              justifyContent: "center",
+            }}
+          >
+            {[
+              {
+                src: "src/assets/img/Roedores.img/casinha-para-mini-coelho-e-porquinho-da-india-chinchila.webp",
+                alt: "Acessorio 1",
+                info: [
+                  "Casinha Para Mini Coelho E Porquinho Da Índia Chinchila",
+                  "Preço: 179,99",
+                ],
+              },
+              {
+                src: "src/assets/img/Roedores.img/Gaiola2.jpg",
+                alt: "Acessorio 2",
+                info: [
+                  " Gaiola Hamster Tubo Super Luxo 3 Andares Diversão Durável ",
+                  "Preço: 189,99",
+                ],
+              },
+              {
+                src: "src/assets/img/Roedores.img/Gaiola3.jpg",
+                alt: "Acessorio 3",
+                info: [
+                  " Gaiola Hamster Roedores 2 Andares Teto Plástico Conforto Qualidade ",
+                  "Preço: 109,99",
+                ],
+              },
+              {
+                src: "src/assets/img/Roedores.img/kit-casinha-porta-feno-e-brinquedo-para-mini-coelhos.webp",
+                alt: "Acessorio 4",
+                info: [
+                  "Kit Casinha Porta Feno E Brinquedo Para Mini Coelhos",
+                  "Preço: 78,90",
+                ],
+              },
+              {
+                src: "src/assets/img/Roedores.img/Acessorio5.jpg",
+                alt: "Acessorio  5",
+                info: [
+                  "Golden Pets Carretel Médio brinquedo para roedores totem",
+                  "Preço:69,90 ",
+                ],
+              },
+            ].map((item, index) => (
+              <div
+                key={index}
+                style={{
+                  width: "320px",
+                  height: "auto",
+                  backgroundColor: "white",
+                  borderRadius: "10px",
+                  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  padding: "15px",
+                }}
+              >
+                <img
+                  src={item.src}
+                  alt={item.alt}
+                  style={{
+                    width: "250px",
+                    height: "250px",
+                    objectFit: "cover",
+                  }}
+                />
+                <ul
+                  style={{
+                    textAlign: "left",
+                    fontSize: "14px",
+                    color: "#333",
+                    padding: "10px",
+                    listStyleType: "none",
+                  }}
+                >
+                  {item.info.map((infoItem, infoIndex) => (
+                    <li key={infoIndex}>
+                      {infoItem.startsWith("Preço:") ? (
+                        <span style={{ fontWeight: "bold", color: "red" }}>{infoItem}</span>
+                      ) : (
+                        infoItem
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
         </div>
       </div>
     </>
